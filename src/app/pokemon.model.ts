56d2e2b4710523cd1f9ce6634f138cec07c31370
pierrebrggn <pierre.bourguigneau@gmail.com>
2025-01-25 15:29:07 +0100

    upload project

---
+++ src/app/pokemon.model.ts
@@ -0,0 +1,10 @@
+export interface Pokemon {
+    id: number;
+    name: string;
+    picture: string;
+    life: number;
+    damage: number;
+    types: [string, string?, string?];
+    created: Date;
+  }
+  export type PokemonList = Pokemon[];
